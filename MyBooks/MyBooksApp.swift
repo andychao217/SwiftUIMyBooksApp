7eaf03e89849eee2fb68300758aa78ec6568a583
Andy Chao <zhaoqing@spon.com.cn>
2024-12-16 10:19:57 +0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Andy Chao on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
